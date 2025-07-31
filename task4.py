cae5fbbf50810141bf6dd2fcd1e2cb031f5979b5
durgaraja09 <durgaraja0917@gmail.com>
2025-07-31 14:52:51 +0530

    Create task4.py
    bank

---
+++ task4.py
@@ -0,0 +1,13 @@
+print("Select the services you want:")
+print(1.'Withdraw'\n2.'deposite\n3.'savings'\n4.'balance')
+service==int(input("Kindly select"))
+if service==1:
+      print("Withdraw")
+elif service==2:
+      print("Deposite")
+elif service==3:
+      print("savings")
+elif service==4:
+      print("Balance")
+else:
+      print("Kindly select the above services")
